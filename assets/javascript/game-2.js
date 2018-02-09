515a109ebca08a751f04266422d07328c7b4e092
Hayden Roberts <haydenroberts@Haydens-MacBook-Pro.local>
2018-02-08 22:44:30 -0800

    Fixed amount of guesses and guessed letters

--- assets/javascript/game-2.js
+++ assets/javascript/game-2.js
@@ -70,79 +70,16 @@ document.onkeyup = function (event) {
     if (isLetterInWord(userInput, splitRandomWord) === (-1)) {
         if (letters.includes(userInput)) {
             console.log("This letter is already here")
+        } else if (storeLetter(userInput, splitRandomWord)) {
+            //console.log(i);
         } else {
             letters.push(userInput);
             guessedLetters.innerHTML = letters;
             console.log("THIS WORKS!")
-            //console.log(i);
+            guessAmount.textContent = guessCount--;
         }
     } else {
         console.log("THIS WORKS!")
         // console.log(i);
     }
-
-    if (storeLetter(userInput, splitRandomWord)) {
-
-    } else {
-        letters.push(userInput);
-        guessedLetters.innerHTML = letters;
-        guessAmount.textContent = guessCount--;
-
-    }
-
-    //     // console.log(allWords[0]);
-    //     if (allWords[0] === "ball") {
-    //         if (userInput === "b") {
-    //             //word.replace('_', 'b');
-    //             splitWord.splice(0, 1, 'b');
-    //             splitWord.join("");
-    //             //console.log(splitWord);
-    //             currentWord.innerHTML = splitWord;
-
-
-    //         } else if (userInput === "a") {
-    //             // currentWord.innerHTML = (word[2] = allWords[0][1]);
-    //             splitWord.splice(2, 1, 'a');
-    //             currentWord.innerHTML = splitWord;
-
-    //             //console.log(splitWord);
-
-    //         } else if (userInput === "l") {
-    //             splitWord.splice(4, 1, 'l');
-    //             splitWord.splice(6, 1, 'l');
-    //             currentWord.innerHTML = splitWord;
-    //         } else {
-    //             // console.log(userInput);
-    //             // console.log(letters);
-    //             // Stores key pressed and guesses get subtracted by 1
-    //             if (letters.includes(userInput)) {
-    //                 console.log("HEY DUDE!")
-    //             } else {
-    //                 letters.push(userInput);
-    //                 guessedLetters.innerHTML = letters;
-    //                 //Subtract 1 from guesses if user guess wrong
-    //                 guessAmount.textContent = guessCount--;
-    //                 if (guessCount === 0) {
-    //                     reset();
-    //                 } else {
-
-    //                 }
-    //             }
-    //             // guessedLetters.innerHTML = event.key;
-    //             // guessAmount.innerHTML = guessCount--;
-    //         }
-
-
-    //     } else {
-
-    //     }
-}
-
-function reset() {
-    for (i = 0; i < splitRandomWord.length; i++) {
-        word += "_ ";
-    };
-    guessCount = 10;
-    letters = [];
-
 }
